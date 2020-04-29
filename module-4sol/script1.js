1e3813d1db4d50b3e1b492ab06ca35d997a0ad3d
Harika D <harika1218@gmail.com>
2020-04-29 17:12:52 +0530

    first js

---
+++ module-4sol/script1.js
@@ -0,0 +1,8 @@
+var arr=["Jadhav","Kohli","Pujara","Dhoni","Josh","Joe"];
+for(var i=0;i<arr.length;i++){
+	if(arr[i][0]=='j' || arr[i][0]=='J')
+		console.log("Goodbye "+arr[i]);
+	else
+		console.log("Hello "+arr[i]);
+}
+
